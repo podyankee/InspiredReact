05ceff294d6486512cc2342199dae2d44a7489d1
podyankee <podyankee@gmail.com>
2023-07-08 09:41:37 +0300

    day 3 1

--- src/Components/Header/Navigation/Category/Category.jsx
+++ src/Components/Header/Navigation/Category/Category.jsx
@@ -1,1 +1,23 @@
-export const Category = ({ list }) => <div>Category</div>;
+import { useLocation, NavLink } from 'react-router-dom';
+import s from './Category.module.scss';
+import cn from 'classnames';
+
+export const Category = ({ list }) => {
+	const location = useLocation();
+	const gender = location.pathname.split('/')[1] || 'women';
+	const categoriesList = list.find(item => item.link === gender);
+
+	return (
+		<ul className={s.category}>
+			{categoriesList.categories.map(item => (
+				<li key={item.link} className={s.item}>
+					<NavLink
+						className={({ isActive }) => cn(s.link, isActive && s.linkActive)}
+						to={`${gender}/${item.link}`}>
+						{item.title}
+					</NavLink>
+				</li>
+			))}
+		</ul>
+	);
+};
