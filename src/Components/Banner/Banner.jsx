32f37567cf2aff1155a79de6e6f4f1bb749e650d
podyankee <podyankee@gmail.com>
2023-07-19 04:40:34 +0300

    first variant of background

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,21 +2,47 @@ import s from './Banner.module.scss';
 import { Container } from '../Layout/Container/Container';
 import { NavLink } from 'react-router-dom';
 import { API_URL } from '../../const';
+import { useEffect, useState } from 'react';
 
-export const Banner = ({ data }) =>
-	data && (
-		<section
-			className={s.banner}
-			style={{
-				backgroundImage: `url(${API_URL}${data.bg.desktop})`,
-			}}>
-			<Container>
-				<div className={s.content}>
-					<h2 className={s.title}>{data.description}</h2>
-					<NavLink className={s.link} to={`/product/${data.id}`}>
-						Перейти
-					</NavLink>
-				</div>
-			</Container>
-		</section>
+export const Banner = ({ data }) => {
+	const [bgURL, setBgURL] = useState('');
+
+	useEffect(() => {
+		const foo = () => {
+			const width = document.documentElement.clientWidth;
+			if (width < 540) {
+				setBgURL(`${API_URL}${data?.bg.mobile}`);
+			} else if (width < 768) {
+				setBgURL(`${API_URL}${data?.bg.tablet}`);
+			} else if (width < 1024) {
+				setBgURL(`${API_URL}${data?.bg.laptop}`);
+			} else {
+				setBgURL(`${API_URL}${data?.bg.desktop}`);
+			}
+		};
+
+		window.addEventListener('resize', foo);
+		foo();
+		return () => {
+			window.removeEventListener('resize', foo);
+		};
+	}, [data]);
+	return (
+		data && (
+			<section
+				className={s.banner}
+				style={{
+					backgroundImage: `url(${bgURL})`,
+				}}>
+				<Container>
+					<div className={s.content}>
+						<h2 className={s.title}>{data.description}</h2>
+						<NavLink className={s.link} to={`/product/${data.id}`}>
+							Перейти
+						</NavLink>
+					</div>
+				</Container>
+			</section>
+		)
 	);
+};
--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -10,12 +10,18 @@ export const MainPage = () => {
 	const { gender, category } = useParams();
 	const dispatch = useDispatch();
 
-	const { activeGender, categories } = useSelector(state => state.navigation);
+	const { activeGender, categories, genderList } = useSelector(state => state.navigation);
 	const genderData = categories[activeGender];
+	const categoryData = genderData?.list.find(item => item.slug === category);
 
 	useEffect(() => {
-		dispatch(setActiveGender(gender));
-	}, [gender, dispatch]);
+		if (gender) {
+			dispatch(setActiveGender(gender));
+		} else if (genderList[0]) {
+			dispatch(setActiveGender(genderList[0]));
+			dispatch(fetchGender(genderList[0]));
+		}
+	}, [gender, genderList, dispatch]);
 
 	useEffect(() => {
 		if (gender && category) {
@@ -30,8 +36,8 @@ export const MainPage = () => {
 
 	return (
 		<>
-			<Banner data={genderData?.banner} />
-			<Goods categoryData={genderData?.list.find(item => item.slug === category)} />
+			{!category && <Banner data={genderData?.banner} />}
+			<Goods categoryData={categoryData} />
 		</>
 	);
 };
