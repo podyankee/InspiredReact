c9d05ffd4da5d0d99727267bc791ea9a8778eb5b
podyankee <podyankee@gmail.com>
2023-07-22 05:51:13 +0300

    lesson 6 finished

---
+++ src/Components/ProductPage/ProductPage.jsx
@@ -0,0 +1,79 @@
+import s from './ProductPage.module.scss';
+import cn from 'classnames';
+import { Container } from '../Layout/Container/Container';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchProduct } from '../../features/productSlice';
+import { API_URL } from '../../const';
+import { ColorList } from '../ColorList/ColorList';
+import { ReactComponent as Like } from '../../assets/heart.svg';
+import { Count } from '../Count/Count';
+
+export const ProductPage = () => {
+	const dispatch = useDispatch();
+	const { id } = useParams();
+	const { product } = useSelector(state => state.product);
+	const [count, setCount] = useState(1);
+	const [selectedColor, setSelectedColor] = useState('');
+
+	const handleIncrement = () => {
+		setCount(prevCount => prevCount + 1);
+	};
+	const handleDecrement = () => {
+		if (count > 1) {
+			setCount(prevCount => prevCount - 1);
+		}
+	};
+
+	const handleColorChange = e => {
+		setSelectedColor(e.target.value);
+	};
+
+	useEffect(() => {
+		dispatch(fetchProduct(id));
+	}, [id, dispatch]);
+
+	return (
+		<section>
+			<Container className={s.container}>
+				<img src={`${API_URL}${product.pic}`} alt={`${product.title} ${product.description}`} />
+				<form className={s.content}>
+					<h2 className={s.title}>{product.title}</h2>
+
+					<p className={s.price}>руб {product.price}</p>
+					<div className={s.vendorCode}>
+						<span className={s.subtitle}>Артикул</span>
+						<span className={s.id}>{product.id}</span>
+					</div>
+					<div className={s.color}>
+						<p className={cn(s.subtitle, s.colorTile)}>Цвет</p>
+						<ColorList
+							colors={product.colors}
+							selectedColor={selectedColor}
+							handleColorChange={handleColorChange}
+						/>
+					</div>
+					<div className={s.description}>
+						<p className={cn(s.subtitle, s.descriptionTitle)}>Описание</p>
+						<p className={s.descriptionText}>{product.description}</p>
+					</div>
+					<div className={s.control}>
+						<Count
+							className={s.control}
+							count={count}
+							handleIncrement={handleIncrement}
+							handleDecrement={handleDecrement}
+						/>
+						<button className={s.addCart} type='submit'>
+							В корзину
+						</button>
+						<button className={s.favorite} aria-label='Добавить в избранное' type='button'>
+							<Like />
+						</button>
+					</div>
+				</form>
+			</Container>
+		</section>
+	);
+};
