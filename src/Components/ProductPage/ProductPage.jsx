e0e1a030882eeec09beb649cf940d0be404dc8f3
podyankee <podyankee@gmail.com>
2023-07-23 07:00:06 +0300

    day 7 homework

--- src/Components/ProductPage/ProductPage.jsx
+++ src/Components/ProductPage/ProductPage.jsx
@@ -9,13 +9,18 @@ import { API_URL } from '../../const';
 import { ColorList } from '../ColorList/ColorList';
 import { ReactComponent as Like } from '../../assets/heart.svg';
 import { Count } from '../Count/Count';
+import { ProductSize } from './ProductSize/ProductSize';
+import { Goods } from '../Goods/Goods';
+import { fetchCategory } from '../../features/goodsSlice';
 
 export const ProductPage = () => {
 	const dispatch = useDispatch();
 	const { id } = useParams();
 	const { product } = useSelector(state => state.product);
+	const { gender, category } = product;
 	const [count, setCount] = useState(1);
 	const [selectedColor, setSelectedColor] = useState('');
+	const [selectedSize, setSelectedSize] = useState('');
 
 	const handleIncrement = () => {
 		setCount(prevCount => prevCount + 1);
@@ -29,51 +34,70 @@ export const ProductPage = () => {
 	const handleColorChange = e => {
 		setSelectedColor(e.target.value);
 	};
+	const handleSizeChange = e => {
+		setSelectedSize(e.target.value);
+	};
 
 	useEffect(() => {
 		dispatch(fetchProduct(id));
 	}, [id, dispatch]);
 
-	return (
-		<section>
-			<Container className={s.container}>
-				<img src={`${API_URL}${product.pic}`} alt={`${product.title} ${product.description}`} />
-				<form className={s.content}>
-					<h2 className={s.title}>{product.title}</h2>
+	useEffect(() => {
+		dispatch(fetchCategory({ gender, category, count: 4, top: true, exclude: id }));
+	}, [gender, category, dispatch, id]);
 
-					<p className={s.price}>руб {product.price}</p>
-					<div className={s.vendorCode}>
-						<span className={s.subtitle}>Артикул</span>
-						<span className={s.id}>{product.id}</span>
-					</div>
-					<div className={s.color}>
-						<p className={cn(s.subtitle, s.colorTile)}>Цвет</p>
-						<ColorList
-							colors={product.colors}
-							selectedColor={selectedColor}
-							handleColorChange={handleColorChange}
+	return (
+		<>
+			<section className={s.card}>
+				<Container className={s.container}>
+					<img
+						className={s.image}
+						src={`${API_URL}${product.pic}`}
+						alt={`${product.title} ${product.description}`}
+					/>
+					<form className={s.content}>
+						<h2 className={s.title}>{product.title}</h2>
+
+						<p className={s.price}>руб {product.price}</p>
+						<div className={s.vendorCode}>
+							<span className={s.subtitle}>Артикул</span>
+							<span className={s.id}>{product.id}</span>
+						</div>
+						<div className={s.color}>
+							<p className={cn(s.subtitle, s.colorTile)}>Цвет</p>
+							<ColorList
+								colors={product.colors}
+								selectedColor={selectedColor}
+								handleColorChange={handleColorChange}
+							/>
+						</div>
+						<ProductSize
+							size={product.size}
+							selectedSize={selectedSize}
+							handleSizeChange={handleSizeChange}
 						/>
-					</div>
-					<div className={s.description}>
-						<p className={cn(s.subtitle, s.descriptionTitle)}>Описание</p>
-						<p className={s.descriptionText}>{product.description}</p>
-					</div>
-					<div className={s.control}>
-						<Count
-							className={s.control}
-							count={count}
-							handleIncrement={handleIncrement}
-							handleDecrement={handleDecrement}
-						/>
-						<button className={s.addCart} type='submit'>
-							В корзину
-						</button>
-						<button className={s.favorite} aria-label='Добавить в избранное' type='button'>
-							<Like />
-						</button>
-					</div>
-				</form>
-			</Container>
-		</section>
+						<div className={s.description}>
+							<p className={cn(s.subtitle, s.descriptionTitle)}>Описание</p>
+							<p className={s.descriptionText}>{product.description}</p>
+						</div>
+						<div className={s.control}>
+							<Count
+								className={s.control}
+								count={count}
+								handleIncrement={handleIncrement}
+								handleDecrement={handleDecrement}
+							/>
+							<button className={s.addCart} type='submit'>
+								В корзину
+							</button>
+							<button className={s.favorite} aria-label='Добавить в избранное' type='button'>
+								<Like />
+							</button>
+						</div>
+					</form>
+				</Container>
+			</section>
+			<Goods title='Вам также может понравиться' />
+		</>
 	);
 };
