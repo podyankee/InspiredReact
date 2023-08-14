613ba35440ca0162e887c87bfdcfc6eae7bdc7a3
podyankee <podyankee@gmail.com>
2023-08-14 21:34:02 +0300

    lesson 9 search, order

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import { Navigation } from './Navigation/Navigation.jsx';
 import { Top } from './Top/Top.jsx';
 import s from './Header.module.scss';
+import { Search } from '../Search/Search';
 
 export const Header = () => (
 	<header className={s.header}>
 		<Top />
+		<Search />
 		<Navigation />
 	</header>
 );
---
+++ src/Components/Search/Search.jsx
@@ -0,0 +1,21 @@
+import s from './Search.module.scss';
+import { Container } from '../Layout/Container/Container';
+import { useSelector } from 'react-redux';
+
+export const Search = () => {
+	const { openSearch } = useSelector(state => state.search);
+	return (
+		openSearch && (
+			<div className={s.search}>
+				<Container>
+					<form className={s.form}>
+						<input className={s.input} type='search' name='search' placeholder='Найти...' />
+						<button className={s.btn} type='submit'>
+							Искать
+						</button>
+					</form>
+				</Container>
+			</div>
+		)
+	);
+};
