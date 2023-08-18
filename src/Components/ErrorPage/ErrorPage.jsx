d2d29989bf85a56fca904209a0ba14a5a3b3a692
podyankee <podyankee@gmail.com>
2023-08-18 03:54:57 +0300

    lesson 10 status server

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,44 @@
-import { useRouteError } from 'react-router-dom';
+import { useRouteError, useNavigate, useLocation } from 'react-router-dom';
+import s from './ErrorPage.module.scss';
+import { useEffect, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchNavigation } from '../../features/navigationSlice';
+import { fetchColors } from '../../features/colorSlice';
 
 export const ErrorPage = () => {
-	const error = useRouteError();
+	const routeError = useRouteError();
+	const { status } = useSelector(state => state.statusServer);
+
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	const timerIdRef = useRef(null);
+
+	useEffect(() => {
+		if (status && location.pathname === '/404') {
+			navigate('/');
+		}
+	}, [status, location, navigate]);
+
+	useEffect(() => {
+		if (!status && location.pathname === '/404') {
+			clearInterval();
+
+			timerIdRef.current = setInterval(() => {
+				dispatch(fetchNavigation());
+				dispatch(fetchColors());
+			}, 3000);
+		}
+		return () => {
+			clearInterval(timerIdRef.current);
+		};
+	}, [dispatch, status, location]);
 
 	return (
-		<div>
-			<h2>Ошибка 404</h2>
-			<p>{error?.message ?? 'Неизвестная ошибка.'}</p>
+		<div className={s.error}>
+			<h2 className={s.title}>Произошла ошибка, попробуйте зайти позже.</h2>
+			<p className={s.message}>{routeError?.message ?? 'Неизвестная ошибка.'}</p>
 		</div>
 	);
 };
